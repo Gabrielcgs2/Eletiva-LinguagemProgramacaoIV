ad7f954bfc4cccc063e8c090a1364dfece63547d
Gabrielcgs2 <99516072+Gabrielcgs2@users.noreply.github.com>
2022-04-07 20:07:02 -0300

    Create resultado.php

---
+++ ListaExercicios1/Ex1/resultado.php
@@ -0,0 +1,11 @@
+<?php
+
+    $valor_produto = $_POST['valor_produto'];
+    $valor_pago = $_POST['valor_pago'];
+
+    if($valor_produto == $valor_pago)
+        echo "Não há troco!";
+    elseif($valor_produto > $valor_pago)
+        echo "Valor pago insulficiente!";
+    else
+        echo "Valor do troco:".($valor_pago - $valor_produto);
